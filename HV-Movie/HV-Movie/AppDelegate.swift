3d671c9663898c489d803f252cb3b1df3c094795
Huseyin Vural <huseyin.vural@live.com>
2024-08-25 15:55:52 +0300

    Set apperance.

--- HV-Movie/HV-Movie/AppDelegate.swift
+++ HV-Movie/HV-Movie/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+        window?.overrideUserInterfaceStyle = .dark
         dependencyManager.registerDependencies()
 
         appCoordinator = AppCoordinator(window: window!)
