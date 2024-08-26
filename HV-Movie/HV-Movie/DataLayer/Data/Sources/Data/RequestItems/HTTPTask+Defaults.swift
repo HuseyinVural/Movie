1178427d2547aa1693f68d122573432618da4691
Huseyin Vural <huseyin.vural@live.com>
2024-08-26 08:21:39 +0300

    Add device current lang code to requests.

--- HV-Movie/HV-Movie/DataLayer/Data/Sources/Data/RequestItems/HTTPTask+Defaults.swift
+++ HV-Movie/HV-Movie/DataLayer/Data/Sources/Data/RequestItems/HTTPTask+Defaults.swift
@@ -17,7 +17,12 @@ extension HTTPTask {
     
     /// I am not adding more here as there is no language switching option at the moment. I have added it to be open for expansion.
     var defaultParameters: [String: Any]? {
-        return ["language": "en-US", "api_key": "fd2b04342048fa2d5f728561866ad52a"]
+        let currentLocale = Locale.current
+        let languageCode = currentLocale.languageCode ?? "en"
+        let regionCode = currentLocale.regionCode ?? "US"
+        let localeIdentifier = "\(languageCode)-\(regionCode)"
+        
+        return ["language": localeIdentifier, "api_key": "fd2b04342048fa2d5f728561866ad52a"]
     }
     
     var authorization: AuthorizationType {
